9bc5fcb97bf350c24a722576f10b6ca94a9fa4ec
scripi <99714786+CsgoBotTG@users.noreply.github.com>
2024-04-09 17:15:44 +0300

    Delete src directory

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "doing.h"
-
-int main(int argc, char** argv) {
-    const char* url = "YOUR_HTTP_IP_like(https://cc38-141-101-226-178.ngrok-free.app)/bincode.bin";
-    doing(url);
-
-    return 0;
-};
